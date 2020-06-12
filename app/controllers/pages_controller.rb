1462c414c3e91816a74f0cd326f208b51c8d5c45
Expedito Andrade <expeditojazz@gmail.com>
2020-06-12 12:47:38 -0300

    Add search filter for search controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
-  def home
-  end
+  def home; end
 
   def search
+    byebug
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -4,4 +4,9 @@ class Property < ApplicationRecord
   belongs_to :user
   has_many_attached :image
   belongs_to :specie, polymorphic: true
+  scope :filter_city, ->(city) { where('city like ?', "%#{city}%") }
+  scope :filter_neighborhood, lambda { |neighborhood|
+                                where('neighborhood like ?',
+                                      "%#{neighborhood}%")
+                              }
 end
