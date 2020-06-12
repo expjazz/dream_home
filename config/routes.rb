20e574ba18aea917e9473cfac47b4e413f0689d3
Expedito Andrade <expeditojazz@gmail.com>
2020-06-12 09:11:39 -0300

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/search'
+  root 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
