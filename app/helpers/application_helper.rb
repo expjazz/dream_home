737c565ec4e32589e7d1085484b311cab572edb3
Expedito Andrade <expeditojazz@gmail.com>
2020-06-13 17:21:10 -0300

    Add font awesome icons. Add navbar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def signed_in?
+    if user_signed_in?
+      render 'layouts/signed_in'
+    else
+      render 'layouts/not_signed_in'
+    end
+  end
 end
